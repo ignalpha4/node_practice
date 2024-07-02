c7d932b27db37a33f6072e7801817a2401db0870
Shubham Bankar <103604945+ignalpha4@users.noreply.github.com>
2024-07-02 23:29:22 +0530

    Update index.js

--- E-mart/index.js
+++ E-mart/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import {userRoutes} from "./src/routes/userRoutes.js";
 import {connect_db} from "./src/db/db_connect.js"
 
+
+
 const app = express();
 const PORT = 5000;
 
